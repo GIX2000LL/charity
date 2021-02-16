6c528436e312fe0f6039f13830f0470d25347406
GIX2000LL <leczyckilukasz@op.pl>
2021-02-16 19:58:00 +0100

    Initial commit

---
+++ src/main/java/pl/coderslab/charity/AppConfig.java
@@ -0,0 +1,37 @@
+package pl.coderslab.charity;
+
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
+
+
+@Configuration
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:validationMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    public Validator validator() {
+        LocalValidatorFactoryBean lvfb = new LocalValidatorFactoryBean();
+        lvfb.setValidationMessageSource(messageSource());
+        return lvfb;
+    }
+
+}
