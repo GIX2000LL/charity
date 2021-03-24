ffbd04bebe02497ad1c27e779ad5536a9f2f463b
GIX2000LL <leczyckilukasz@op.pl>
2021-03-24 23:14:43 +0100

    Heroku

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -2,12 +2,20 @@ package pl.coderslab.charity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CharityApplication {
+public class CharityApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder applicationBuilder) {
+
+        return applicationBuilder.sources(CharityApplication.class);
     }
 
 }
